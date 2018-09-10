034034f3b0862e5b5824fde08e9580299b69d54a
puckbee <puckbee@gmail.com>
2018-09-10 09:42:19 +0800

    refresh

---
+++ header.js
@@ -0,0 +1,11 @@
+document.write('\
+<div>\
+<a href="http://web-supersoulhunter.rhcloud.com" target="_self"><img src="logo.png" style="margin-top: 5px;" height="80px"></a>\
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\
+<div class="icon"><a href="index.html"><img src="home.svg">Home</a></div>\
+<div class="icon"><a href="index.html"><img src="research.svg">Research</a></div>\
+<div class="icon"><a href="index.html"><img src="publications.svg">Publications</a></div>\
+<div class="icon"><a href="index.html"><img src="hobby.svg">Hobbies</a></div>\
+</div>\
+');
